43d2cd6d6efd7f70c10dfb681a44601edef7523a
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-10-01 16:11:06 +0200

    first deploy 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import axios from 'axios'
 import './App.css';
-import PortfolioItem from './components/PortfolioItem'
+// import PortfolioItem from './components/PortfolioItem'
 //Our Goals
 //Given the portfolio item, render it on screen
 //Make a AJAX request to get the portfolio items from the server
 
-const exampleEntries = [
-    {
-        "id": 1,
-        "title": "Everything is Gold",
-        "description": "Life is gold like a machine, \nbuy a watch just to flex.",
-        "background": "#ffd700",
-        "created_at": "2020-01-16T22:54:37.977Z",
-        "updated_at": "2020-01-16T22:54:47.844Z"
-    },
-    {
-        "id": 2,
-        "title": "Red is Passion",
-        "description": "Passion is fire\nFire is lit\nPoop emoji",
-        "background": "#f03434",
-        "created_at": "2020-01-16T22:55:12.410Z",
-        "updated_at": "2020-01-16T22:55:12.414Z"
-    }
-]
+// const exampleEntries = [
+//     {
+//         "id": 1,
+//         "title": "Everything is Gold",
+//         "description": "Life is gold like a machine, \nbuy a watch just to flex.",
+//         "background": "#ffd700",
+//         "created_at": "2020-01-16T22:54:37.977Z",
+//         "updated_at": "2020-01-16T22:54:47.844Z"
+//     },
+//     {
+//         "id": 2,
+//         "title": "Red is Passion",
+//         "description": "Passion is fire\nFire is lit\nPoop emoji",
+//         "background": "#f03434",
+//         "created_at": "2020-01-16T22:55:12.410Z",
+//         "updated_at": "2020-01-16T22:55:12.414Z"
+//     }
+// ]
 
 class App extends React.Component{
   state = {
